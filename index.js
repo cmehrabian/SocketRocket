8cdae7686a89d9f0eb77a8e3a1c24e8cb01bf71f
cmehrabian <cameron.mehrabian@gmail.com>
2015-02-11 00:49:58 -0800

    Tuts Working

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
- res.sendFile('index.html');
+ res.sendFile(__dirname +'/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-   console.log('message: ' + msg);
+   io.emit('chat message', msg);
   });
 });
 
